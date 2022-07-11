0a868e6dc0a2a7c44dfabcb467358030e503a822
xSylla <syllamodder@gmail.com>
2022-07-11 02:07:03 +0200

    sm8250-common: rootdir: Add support for lito CPU configuration
    Change-Id: I94e1c6338859bc71a20a5569e237c9a8cf8f6700

--- rootdir/bin/init.qti.dcvs.sh
+++ rootdir/bin/init.qti.dcvs.sh
@@ -28,7 +28,10 @@
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+ProductName=$(getprop ro.board.platform)
+
 for device in /sys/devices/platform/soc; do
+    #Enable mem_latency governor for L3, LLCC, and DDR scaling
     for memlat in $device/*qcom,devfreq-l3/*cpu*-lat/devfreq/*cpu*-lat; do
         echo "mem_latency" >$memlat/governor
         echo 8 >$memlat/polling_interval
@@ -43,28 +46,58 @@ for device in /sys/devices/platform/soc; do
     done
 
     #Enable compute governor for gold latfloor
-    for latfloor in $device/*cpu-ddr-latfloor*/devfreq/*cpu-ddr-latfloor*; do
+    for latfloor in $device/*cpu*-ddr-latfloor*/devfreq/*cpu-ddr-latfloor*; do
         echo "compute" >$latfloor/governor
         echo 8 >$latfloor/polling_interval
     done
 
-    for qoslat in $device/*qoslat/devfreq/*qoslat; do
-        echo "mem_latency" >$qoslat/governor
-        echo 10 >$qoslat/polling_interval
-        echo 50 >$qoslat/mem_latency/ratio_ceil
-    done
+    if [ "$ProductName" = "kona" ]; then
+        for qoslat in $device/*qoslat/devfreq/*qoslat; do
+            echo "mem_latency" >$qoslat/governor
+            echo 10 >$qoslat/polling_interval
+            echo 50 >$qoslat/mem_latency/ratio_ceil
+        done
+    fi
 
+    #Enable cdspl3 governor for L3 cdsp nodes
     for l3cdsp in $device/*qcom,devfreq-l3/*cdsp-l3-lat/devfreq/*cdsp-l3-lat; do
         echo "cdspl3" >$l3cdsp/governor
     done
 
     #Gold L3 ratio ceil
-    for l3gold in $device/*qcom,devfreq-l3/*cpu4-cpu-l3-lat/devfreq/*cpu4-cpu-l3-lat; do
-        echo 4000 >$l3gold/mem_latency/ratio_ceil
-    done
+    if [ "$ProductName" = "kona" ]; then
+        for l3gold in $device/*qcom,devfreq-l3/*cpu4-cpu-l3-lat/devfreq/*cpu4-cpu-l3-lat; do
+            echo 4000 >$l3gold/mem_latency/ratio_ceil
+        done
+    elif [ "$ProductName" = "lito" ]; then
+        for l3gold in $device/*qcom,devfreq-l3/*cpu6-cpu-l3-lat/devfreq/*cpu6-cpu-l3-lat; do
+            echo 4000 >$l3gold/mem_latency/ratio_ceil
+        done
+    fi
 
     #Prime L3 ratio ceil
     for l3prime in $device/*qcom,devfreq-l3/*cpu7-cpu-l3-lat/devfreq/*cpu7-cpu-l3-lat; do
-        echo 20000 >$l3prime/mem_latency/ratio_ceil
+        if [ "$ProductName" = "kona" ]; then
+            echo 20000 >$l3prime/mem_latency/ratio_ceil
+        elif [ "$ProductName" = "lito" ]; then
+            echo 4000 >$l3prime/mem_latency/ratio_ceil
+        fi
     done
 done
+
+if [ -f /sys/devices/soc0/soc_id ]; then
+    soc_id=$(cat /sys/devices/soc0/soc_id)
+else
+    soc_id=$(cat /sys/devices/system/soc/soc0/id)
+fi
+
+if [ "$ProductName" = "lito" ]; then
+    case "$soc_id" in
+    "434" | "459")
+        for gold_memlat in $device/*qcom,devfreq-l3/*cpu6*-lat/devfreq/*cpu6*-lat; do
+            echo 25000 >$gold_memlat/mem_latency/wb_filter_ratio
+            echo 60 >$gold_memlat/mem_latency/wb_pct_thres
+        done
+        ;;
+    esac
+fi
